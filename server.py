cf9163701ae142c4414c24b8be1b756044833a9e
Samrat Thapa <samratthapa120@gmail.com>
2021-05-18 23:29:18 +0900

    server2

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import flask
-from flask import jsonify,request
-import time,glob,path
+from flask import jsonify,request,json
+import time,glob
+import pandas as pd
 
 __name_ = "lottery"
 
 app = flask.Flask(__name_)
 app.config["DEBUG"] = True
 
-DEF_TIME = 30*24*3600 #By default, we use data of past month)
+DEF_PERIOD = 30*24*3600 #By default, we use data of past month)
 
 # Load all the data
 PLANETS_PATH = "/LotteryProject/Planets_eng.xlsx"
@@ -15,15 +16,16 @@ MOON_PATH = "/LotteryProject/Moon Cycles Result.xlsx"
 LOTTERY_PATH = "/LotteryProject/LotteryData/"
 
 _dataplanet = pd.read_excel(PLANETS_PATH)
-moon_data = pd.read_excel(MOON_DATA)
+moon_data = pd.read_excel(MOON_PATH)
 
 lottery_files = {}
 for file in glob.glob(LOTTERY_PATH+"*"):
-  lottery_files[file.split('.')[0]] = pd.read_csv(path.join(LOTTERY_PATH,file))
+  lottery_files[file.split('.')[0]] = pd.read_csv(LOTTERY_PATH+file)
 
  
 
-def get_information(period,lottery=None)
+def get_information(period,lottery):
+    return None
 
 @app.route('/test',methods=['GET'])
 def test():
@@ -31,13 +33,13 @@ def test():
 
 @app.route('/dashboard',methods=['GET'])
 def get_stats():
-    time = DEF_PERIOD
+    period = DEF_PERIOD
     lottery_type = None
     if 'period' in request.form:
-        time = request.form['period']
+        period = request.form['period']
     if 'lottery_type' in request.form:
         lottery_type = request.form['lottery_type']
-    data = 
+    data = get_information(period,lottery_type)
     response = app.response_class(
         response=json.dumps(data),
         status=200,
